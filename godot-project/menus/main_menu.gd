d961c663a0889f6776b0a2cb780981c20c54d3a5
Daniel Mason <daniel@danielmason.com>
2024-08-16 21:52:58 +0100

    Make version number visible for the sake of checking on itch

--- godot-project/menus/main_menu.gd
+++ godot-project/menus/main_menu.gd
@@ -1,5 +1,8 @@
 extends Control
 
+func _ready() -> void:
+	$VersionLabel.text = "Version: " + ProjectSettings.get_setting("application/config/version")
+
 
 func _on_credits_pressed() -> void:
 	get_tree().change_scene_to_file("res://menus/credits.tscn")
